4e9a7ad9a0b637ee0de95bdac6ec81d0d7ab7320
hyechong <ckgpwl2@gmail.com>
2023-07-11 07:48:37 +0900

    Cleanup 0711

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import { useState, useEffect } from 'react';
 function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState('');
+  const [showing, setShowing] = useState(false);
+  // const onClick = () => {
+  //   setValue((prev) => prev + 1);
+  // };
+  function Hello() {
+    useEffect(() => {
+      console.log('hi :)');
+      return () => console.log('bye :(');
+    });
+    return <h1>Hello!</h1>;
+  }
   const onClick = () => {
-    setValue((prev) => prev + 1);
+    setShowing((prev) => !prev);
   };
   const onChange = (event) => {
     setKeyword(event.target.value);
@@ -19,7 +30,7 @@ function App() {
       console.log('search for', keyword);
     }
   }, [keyword]); // [dependency(지켜보려는것)] : dependency가 변화 할 때 코드를 실행, 따라서 [](빈 배열)로 두면 처음 한 번만 실행되는 것임 cf. [keyword, counter]와 같은 형태로 적어 keyword나 counter 둘 중 하나가 변화할 때 코드 실행시킬 수도 있음
-  useEffect(() => {}, [counter]);
+  // useEffect(() => {}, [counter]);
   // console.log('i run all the time');
   // const iRunOnlyOnce = () => {
   //   console.log('i run only once');
@@ -36,7 +47,9 @@ function App() {
         placeholder='Search here...'
       />
       <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {/* <button onClick={onClick}>click me</button> */}
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
     </div>
   );
 }
